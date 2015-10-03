8df6f73235c21a95bf332455633f4d6592cbf6e0
BenJneB <benjamin.daubry@student.uclouvain.be>
2015-10-03 12:55:59 +0200

    ?

--- numberlink.py
+++ numberlink.py
@@ -29,7 +29,8 @@ class NumberLink(Problem):
                                 mapL2.append(col)
                     mapL.append(mapL2)
             print(mapL)
-            self.initial=('init',tuple(mapL))
+            self.initial=mapL
+
 
 ###################### 
 # Auxiliary function #
@@ -38,23 +39,23 @@ class NumberLink(Problem):
 directions = [ [-1, 0], [1, 0], [0, -1], [0, 1] ]
 
 def pathExists(grid, start, end):
-	visited = [ [0 for j in range(0, len(grid[0]))] for i in range(0, len(grid)) ]
-	ok = pathExistsDFS(grid, start, end, visited)
-	return ok
+    visited = [ [0 for j in range(0, len(grid[0]))] for i in range(0, len(grid)) ]
+    ok = pathExistsDFS(grid, start, end, visited)
+    return ok
 
 def pathExistsDFS(grid, start, end, visited):
-	for d in directions:
-		i = start[0] + d[0]
-		j = start[1] + d[1]
-		next = [i, j]
-		if i == end[0] and j == end[1]:
-			return True
-		if inBounds(grid, next) and grid[i][j] == '.' and not visited[i][j]:
-			visited[i][j] = 1
-			exists = pathExistsDFS(grid, next, end, visited)
-			if exists:
-				return True
-	return False
+    for d in directions:
+        i = start[0] + d[0]
+        j = start[1] + d[1]
+        next = [i, j]
+        if i == end[0] and j == end[1]:
+            return True
+        if inBounds(grid, next) and grid[i][j] == '.' and not visited[i][j]:
+            visited[i][j] = 1
+            exists = pathExistsDFS(grid, next, end, visited)
+            if exists:
+                return True
+    return False
 
 def inBounds(grid, pos):
   return 0 <= pos[0] and pos[0] < len(grid) and 0 <= pos[1] and pos[1] < len(grid[0])
