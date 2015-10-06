ef34fc27878f9a4d97c27eca0474ce96d034bea3
pfichefet <p.fichefet@gmail.com>
2015-10-06 16:48:50 +0200

    yolo

--- numberlink.py
+++ numberlink.py
@@ -1,4 +1,5 @@
 '''NAMES OF THE AUTHOR(S): TODO'''
+"YOLLLLLLLOOOOOOOOOOO"
 import time
 import sys
 from os import listdir,system
@@ -9,40 +10,40 @@ from search import *
 #################
 
 class NumberLink(Problem):
-    def __init__(self, init):
-            self.createMap(init)
-            print(self.goal_test(self.initial))
-            pass
+	def __init__(self, init):
+		self.createMap(init)
+		print(self.goal_test(self.initial))
+		pass
 	
-    def goal_test(self, state):
-            i=0
-            for e in state[0]:
-                if '.' in e:
-                    return False
-            return True
+	def goal_test(self, state):
+		i=0
+		for e in state[0]:
+			if '.' in e:
+				return False
+		return True
     
-    def successor(self, state):
-	    pass
+	def successor(self, state):
+		pass
 
-    def createMap(self,path):
-            mapL=[]
-            mapLetter=[]
-            f=open(path,'r')
-            y=0
-            for line in f:
-                    mapL2=[]
-                    x=0
-                    for col in line:
-                        if(col!= '\n'):
-                                mapL2.append(col)
-                        if(col!='.' and col!='\n'):
-                            mapLetter.append((col,x,y))
-                        x=x+1
-                    mapL.append(tuple(mapL2))
-                    y=y+1
-            print(mapL)
-            print(mapLetter)
-            self.initial=(tuple(mapL),tuple(mapLetter))
+	def createMap(self,path):
+		mapL=[]
+		mapLetter=[]
+		f=open(path,'r')
+		y=0
+		for line in f:
+			mapL2=[]
+			x=0
+			for col in line:
+				if(col!= '\n'):
+					mapL2.append(col)
+				if(col!='.' and col!='\n'):
+					mapLetter.append((col,x,y))
+				x=x+1
+			mapL.append(tuple(mapL2))
+			y=y+1
+		print(mapL)
+		print(mapLetter)
+		self.initial=(tuple(mapL),tuple(mapLetter))
 
 
 ###################### 
@@ -51,27 +52,66 @@ class NumberLink(Problem):
 
 directions = [ [-1, 0], [1, 0], [0, -1], [0, 1] ]
 
+def TupleToList(Tuple)
+	List = []
+	for line in Tuple:
+		List.append(list(line))
+	return List
+
+def ListToTuple(List)
+	Tuple = []
+	for line in List:
+		Tuple.append(tuple(line))
+	return tuple(Tuple)
+
+def IA(state)
+	grid = TupleToList(state[0])
+	letter = TupleToList(state[1])
+	startPoints = searchLetter(letter,'A')
+	start = endPoints[0]
+	end = endpoints[1]
+	nextState = []
+	if(not pathExists(grid,start,end)):
+		return nextState
+	for choice in directions:
+		grid[start[0]+choice[0]][start[1]+choice[1]]
+		letter
+	
+	
+
+def searchLetter(tab,letter)
+	startPoints = []
+	endPoints = []
+	dico = {}
+	for Tuple in tab:
+		if Tuple[0] == letter:
+			if letter not in dico
+				startPoints.append(Tuple[1])
+				.append(Tuple[2])
+	return (tuple(startPoints),tuple(endPoints))
+		
+
 def pathExists(grid, start, end):
-    visited = [ [0 for j in range(0, len(grid[0]))] for i in range(0, len(grid)) ]
-    ok = pathExistsDFS(grid, start, end, visited)
-    return ok
+	visited = [ [0 for j in range(0, len(grid[0]))] for i in range(0, len(grid)) ]
+	ok = pathExistsDFS(grid, start, end, visited)
+	return ok
 
 def pathExistsDFS(grid, start, end, visited):
-    for d in directions:
-        i = start[0] + d[0]
-        j = start[1] + d[1]
-        next = [i, j]
+	for d in directions:
+		i = start[0] + d[0]
+		j = start[1] + d[1]
+		next = [i, j]
         if i == end[0] and j == end[1]:
-            return True
+		return True
         if inBounds(grid, next) and grid[i][j] == '.' and not visited[i][j]:
-            visited[i][j] = 1
-            exists = pathExistsDFS(grid, next, end, visited)
-            if exists:
-                return True
-    return False
+		visited[i][j] = 1
+		exists = pathExistsDFS(grid, next, end, visited)
+		if exists:
+			return True
+		return False
 
 def inBounds(grid, pos):
-  return 0 <= pos[0] and pos[0] < len(grid) and 0 <= pos[1] and pos[1] < len(grid[0])
+	return 0 <= pos[0] and pos[0] < len(grid) and 0 <= pos[1] and pos[1] < len(grid[0])
 
 #####################
 # Launch the search #
